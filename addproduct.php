96c76b4bb9ec3302b7d78737909118151a35aa7f
istvan789 <71489862+istvan789@users.noreply.github.com>
2022-09-18 19:55:32 +0100

    Update addproduct.php

--- addproduct.php
+++ addproduct.php
@@ -33,8 +33,8 @@ include_once ("setout/header.inc.php");
 
         <div class="form-row">
         <div class="form-group col-md-3">
-             <label for="name1" class="col-form-label">Name</label>
-            <input type="text" name="name1" id="name1" class="form-control form-control-sm" placeholder="Enter Name of Product" required>
+             <label for="name" class="col-form-label">Name</label>
+            <input type="text" name="name" id="name" class="form-control form-control-sm" placeholder="Enter Name of Product" required>
             
         </div>
         </div>
