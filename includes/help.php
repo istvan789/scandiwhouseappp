e708639882f7d06ec99871fa959dc2bd3bf9d353
istvan789 <71489862+istvan789@users.noreply.github.com>
2022-09-18 20:22:31 +0100

    Update help.php

--- includes/help.php
+++ includes/help.php
@@ -1,6 +1,6 @@
 <?php 
 session_start();
-$con = mysqli_connect("eu-cdbr-west-03.cleardb.net","bfa8f0b1dce657","9ff87698","heroku_caa4546c7212293");
+$conn = mysqli_connect("eu-cdbr-west-03.cleardb.net","bfa8f0b1dce657","9ff87698","heroku_caa4546c7212293");
 
 if (isset($_POST['delete-product-btn']))
 {
